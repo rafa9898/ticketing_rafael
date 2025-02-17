3f4d9eb830101d0ac82a2e058a1ee132b1e14adc
rafa9898 <rafa180498@gmail.com>
2025-02-17 20:54:01 +0100

    added new component sessions

---
+++ src/app/pages/sessions/session/session.component.ts
@@ -0,0 +1,84 @@
+import { Component, Input } from '@angular/core';
+import { Session } from '../../../interfaces/session';
+import { FormsModule } from '@angular/forms';
+import { CartServiceService } from '../../../services/cart-service.service';
+import { Event } from '../../../interfaces/event';
+import { ItemCart } from '../../../interfaces/item-cart';
+
+@Component({
+  selector: 'app-session',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './session.component.html',
+  styleUrl: './session.component.scss'
+})
+export class SessionComponent {
+
+  public item_cart: ItemCart = { date: '', tickets: 0, title: '', id: '' }
+
+  @Input() item: Session = {
+
+    availability: '',
+    date: ''
+
+  };
+
+  @Input() event: Event = {
+
+    id: '',
+    title: '',
+    subtitle: '',
+    image: ''
+
+  };
+
+  public tickets: number = 0;
+
+  constructor(private cart_service: CartServiceService) {}
+
+  ngOnInit() {
+
+    this.formatDate();
+
+  }
+
+  public formatDate() {
+
+    const date = new Date(parseInt(this.item.date));
+
+    const formatted_date = date.getDate() + "/" + date.getMonth() + "/" + date.getFullYear();
+
+    this.item.date = formatted_date;
+
+  }
+
+  public addTicket() {
+
+    if(this.tickets < parseInt(this.item.availability)) {
+
+      this.tickets++;
+      
+      this.item_cart.id = this.event.id;
+      this.item_cart.date = this.item.date;
+      this.item_cart.title = this.event.title;
+      this.item_cart.tickets = this.tickets;
+
+      this.cart_service.addTickets(this.item_cart);
+
+    } 
+
+  }
+
+  public substractTicket() {
+
+    if(this.tickets > 0) this.tickets--;
+
+  }
+
+  public verifyAvailability() {
+
+    if((this.tickets > parseInt(this.item.availability)) || (this.tickets < 0)) this.tickets = 0;
+
+  }
+
+}
--- src/app/pages/sessions/sessions.component.ts
+++ src/app/pages/sessions/sessions.component.ts
@@ -4,11 +4,14 @@ import { SessionsService } from '../../services/sessions.service';
 import { Observable } from 'rxjs';
 import { CommonModule } from '@angular/common';
 import { Session } from '../../interfaces/session';
+import { Event } from '../../interfaces/event';
+import { SessionComponent } from './session/session.component';
+import { CartComponent } from '../../components/cart/cart.component';
 
 @Component({
   selector: 'app-sessions',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SessionComponent, CartComponent],
   templateUrl: './sessions.component.html',
   styleUrl: './sessions.component.scss'
 })
@@ -16,6 +19,14 @@ export class SessionsComponent {
 
   public sessions_id: string | null = "";
   public sessions$: Observable<Session[]> = new Observable<Session[]>();
+  public event: Event = {
+
+    id: '',
+    title: '',
+    subtitle: '',
+    image: ''
+
+  }
 
   constructor(private activatedRoute: ActivatedRoute, private sessions_service: SessionsService){}
 
@@ -26,6 +37,7 @@ export class SessionsComponent {
     if(this.sessions_id != null) {
 
       this.sessions$ = this.sessions_service.getSession(this.sessions_id);
+      this.sessions_service.getDataSession(this.sessions_id).subscribe((event: Event) => this.event = event);
 
     }
 
